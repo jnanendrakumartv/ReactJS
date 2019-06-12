ed69321e9d1c3a8d8d320f4620a623ab777eb4a0
jnanendranagar <jvijayakumarostb1@gmail.com>
2019-06-12 10:45:49 +0530

    react_task commited

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
  //import {BrowserRouter as Router, Route, Switch, Redirect} from'react-router-dom';
- //import Ageclass from './Ageclass';
-  //import Cityclass from './Cityclass';
- // import Dobclass from './Dobclass';
+  import Ageclass from './task2/Ageclass';
+  import Cityclass from './task2/Cityclass';
+  // import Dobclass from './task2/Dobclass';
   import Login from './Login';
-  //import Signin from'./Signin';
+  //import Signin from'./task2/Signin';
 //import Login from'./Login';
 
 function App() {
@@ -18,9 +18,9 @@ function App() {
     //   </switch>
     // </Router>
     <div className="App">
-        {/* <Ageclass title='Age'></Ageclass>*/}
-       {/* <Cityclass title='City'></Cityclass> */}
-      {/* <Dobclass title='Date Of Birth'></Dobclass>  */}
+          <Ageclass title='Age'></Ageclass> 
+        <Cityclass title='City'></Cityclass> 
+         {/* <Dobclass title='Date Of Birth'></Dobclass>   */}
 
       <Login/>  
        {/* <Cityclass/> */}
